82c1ba8645e39968220d3ea76d41b86cd97ad2bc
b3d0na <mirrorcode@protonmail.com>
2023-10-20 22:52:50 +0600

    Little update

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ window.addEventListener("copy", function (e) {
 	e.clipboardData &&
 		e.clipboardData.setData("application/json", JSON.stringify(data));
 	e.preventDefault();
-	console.log("Copied suuccesfully...");
+	alert("Copied suuccesfully...");
 });
 
 submitBtn.addEventListener("click", function (e) {
 	e.preventDefault();
-	console.log("Copied..........");
 	document.execCommand("copy");
 });
 // submitBtn.onClick = function (e) {
